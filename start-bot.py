60c0dbca071ecf723c02a7c1a11bfe36598a7d67
Marc-Andre Lemburg <mal@lemburg.com>
2017-05-06 14:31:03 +0200

    First running version.

---
+++ start-bot.py
@@ -0,0 +1,4 @@
+from twitterbot.twitterbot import main
+import twitterbot_config
+
+main(twitterbot_config)
--- twitterbot/twitterbot.py
+++ twitterbot/twitterbot.py
@@ -1,31 +1,47 @@
 """ Telegram Bot - PyDDF Sprint 2017
 
 """
-import telebot
+import telepot
+import telepot.helper
+import telepot.delegate
+from telepot import delegate
+import twitterbot_config
 
 ###
 
 class TwitterBotChatHandler(telepot.helper.ChatHandler):
 
     def __init__(self, *args, **kws):
-        pass
+        super(TwitterBotChatHandler, self).__init__(*args, **kws)
 
     def on_chat_message(self, msg):
-        pass
+        self.sender.sendMessage('Hello world!')
 
 class TwitterBotDelegatorBot(telepot.DelegatorBot):
-    
-    def __init__(self, token, owner_id):
-        pass
 
+    # On-idle timeout in seconds
+    on_idle_timeout = twitterbot_config.ON_IDLE_TIMEOUT
     
+    def __init__(self, token, authorized_users):
+        super(TwitterBotDelegatorBot, self).__init__(
+            token, [
+                # Here is a delegate to specially handle owner commands.
+                delegate.pave_event_space()(
+                    delegate.per_chat_id_in(authorized_users,
+                                            types='private'),
+                    delegate.create_open,
+                    TwitterBotChatHandler,
+                    # Add handler argument here
+                    timeout=self.on_idle_timeout)
+                ]
+            )
 
 ###
 
 def main(config):
     
     bot = TwitterBotDelegatorBot(config.TELEGRAM_BOT_TOKEN,
-                                 config.TELEGRAM_BOT_OWNER_ID)
+                                 config.AUTHORIZED_USERS)
     bot.message_loop(run_forever='TwitterBot listening ...')
 
 ###
