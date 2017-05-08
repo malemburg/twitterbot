e536fd17944891225fdac6bff3ff654563ac286e
Marc-Andre Lemburg <mal@lemburg.com>
2017-05-08 19:31:20 +0200

    Add simple helper scripts to print the user and public stream.

---
+++ read-public-feed.py
@@ -0,0 +1,6 @@
+import sys
+from twitterbot.twitterfeed import *
+import twitterbot_config
+
+print_twitter_feed(twitterbot_config,
+                   domain='stream.twitter.com')
---
+++ read-twitter-feed.py
@@ -0,0 +1,5 @@
+import sys
+from twitterbot.twitterfeed import *
+import twitterbot_config
+
+print_twitter_feed(twitterbot_config)
